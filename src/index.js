933baaacf74f3644c57085bd43a372e416f43b97
Ryan Bartolome <rahhyahh@icloud.com>
2024-08-01 16:46:36 -1000

    updated index.js retrieve form element, attached submit event listener, added preventDefault()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-/*
- Your solution here
- */
+// Select the <form> element
+
+const formElement = document.querySelector("form");
+
+console.log(formElement);
+
+// Attach a submit event listener to the form
+    // Prevent default form submission
+    // Retrieve the input value
+    // Check if the input value is blank (trimmed)
+        // If blank:
+            // Check if the error element already exists
+                // If not, create the error element and append it to the form
+        // If not blank:
+            // Check if the error element exists
+                // If it does, remove it from the form
